921afd7ed8727485bba25506ac18861423fbc1c7
Erik Koppes <41686327+KoppesEA@users.noreply.github.com>
2023-08-04 16:52:46 -0400

    update

---
+++ CastEiJ_Samtools_NUP107peakseq_GRCm38_v68.bash
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#SBATCH -N 1
+#SBATCH -t 1-00:00
+#SBATCH -J Samtools_mGRC38
+#SBATCH --output=CastEiJ_Samtools_out-%A_%a.txt
+#SBATCH --cpus-per-task=8  ## match samtools @
+#SBATCH --mem=12g  ##
+
+## BASH SCRIPT TO EXTRACT GRCm38_68 COORDINATES DEFINED IN BEDFILE FROM REF(B6/C57), CAST AND N-MASKED GENOMES
+
+## Define Variables
+WKDIR=/ix1/mmann/KoppesEA/REF_Sequences/Mus_musculus
+CASTDIR=$WKDIR/Cast_EiJ/CastEiJ_GRCm38
+REF_GRCm38v68=$WKDIR/GRCm38_ref/GRCm38_68/Mus_musculus.GRCm38.68.dna.toplevel.fa  #
+CAST_GRCm38v68=$CASTDIR/CAST_EiJ_full_sequence/Mus_musculus.GRCm38.68.dna.CASTEiJ.fa  ## snp-split generated .fa Cast and N-masked .txt files concatenated
+NMASK_GRCm38v68=$CASTDIR/CAST_EiJ_N-masked/Mus_musculus.GRCm38.68.dna.NMASK.fa
+COORD_LIST=$CASTDIR/Nup107_mm10peak_GRCm38_v68_imprint.txt
+OUT_FA=$CASTDIR/Mmus_GRCm38_68_REFCASTNMASK_Nup107imprint.fa
+
+## Import Nup107 peaks formatted as manually, remove ## comment out lines
+coords=($(cat $COORD_LIST))
+echo -e "Reporting Ref, CAST and N-masked sequences for:\n" $REF_GRCm38v68 "\n" $CAST_GRCm38v68 "\n" $NMASK_GRCm38v68 "\nbased on:\n" $COORD_LIST 
+
+## Load Samtools
+module load gcc/8.2.0
+module load samtools/1.14
+
+#`_ comment out if done before
+## Samtools index .fa from Ref, Cast and N-masked GRCm38_68 snp-split generated genomes
+echo "Indexing REF, CAST and NMASK genomes"
+samtools index $REF_GRCm38v68
+samtools index $CAST_GRCm38v68
+samtools index $NMASK_GRCm38v68
+#`
+
+rm $OUT_FA
+touch $OUT_FA
+echo -e "removing and making:\n" $OUT_FA
+
+## Samtools faidx extract chromosomal coordinates
+while IFS=$'\t' read -r -a rec; do
+    NAME=${rec[0]}
+    COORD=${rec[1]}
+    echo "Processing Record NAME: " $NAME
+    echo "Processing Record COORD: " $COORD
+    echo "Processing Record NAME: " $NAME >> $OUT_FA
+    echo "REF B6_" $NAME >> $OUT_FA
+    samtools faidx $REF_GRCm38v68 $COORD >> $OUT_FA
+    echo "CASTEiJ_" $NAME >> $OUT_FA
+	samtools faidx $CAST_GRCm38v68 $COORD >> $OUT_FA
+	echo "NMASK_" $NAME >> $OUT_FA
+	samtools faidx $NMASK_GRCm38v68 $COORD >> $OUT_FA    
+done < $COORD_LIST
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Mouse_strain_genomeprep_snpsplit
-GRCm38 and GRCm39 strain-specific reference genome generated with snp-split
+GRCm38 and GRCm39 strain-specific reference genome generated with snp-split followed by sequence extraction based on chromosomal coordinates
+Run on GRCm38 to extract ref/cast/nmask sequences at ChIP-Seq peaks (rather than realign old data)  
+Run on GRCm39 to extract ref/cast/nmask sequences at Nucleoporin genes for genome-editing design
 
 ## Summarry
 The following commands were run to download appropriate GRCm38 and GRCm39 genomes and then to generate Castenous (Cast_EiJ) strain-specific genomes. While this largely followed the snpsplit genome gen guide (see references) there were some specific issues getting the backdated GRCm38 to work. This was run July 26th 2023 on the PITT HTC.
@@ -16,8 +18,14 @@ The GRCm38.68 ref was required for the v5 SNP build (other builds did not work),
 ### Run SNPsplit_genome_preparation using SLURM
 Run the Bash scripts `CastEiJ_snpsplit_genomegen_GRCm39.bash` and `CastEiJ_snpsplit_genomegen_GRCm38_v68` including output for strain-specific genome
 
+### Combine chr.fa files for CAST and NMASK
+From within the output Cast and Nmask directories run the following interactive commands:  
+`cat *.fa > Mus_musculus.GRCm38.68.dna.CASTEiJ.fa &`  
+`cat *.fa > Mus_musculus.GRCm38.68.dna.NMASK.fa &`
+
 ### Extraction of Genomic Sequences of regions of interestr of Reference, N-masked and CastEiJ using Samtools
-TBD--inprogress
+Generate .tsv with 2 columns (seqname and coordinates) and no header; manually typed out
+For GRCm38_v68 coordinates run `CastEiJ_Samtools_NUP107peakseq_GRCm38_v68.bash` which utilizes `Nup107_mm10peak_GRCm38_v68_imprint.txt` as input .tsv
 
 ## References and Links
 https://felixkrueger.github.io/SNPsplit/genome_prep/legacy/
