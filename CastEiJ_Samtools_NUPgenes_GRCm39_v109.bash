27449380e086695d8eebe5c0975f2ca1dc790398
Erik Koppes <41686327+KoppesEA@users.noreply.github.com>
2023-08-21 13:23:48 -0400

    Update CastEiJ_Samtools_NUPgenes_GRCm39_v109.bash

--- CastEiJ_Samtools_NUPgenes_GRCm39_v109.bash
+++ CastEiJ_Samtools_NUPgenes_GRCm39_v109.bash
@@ -17,6 +17,13 @@ CAST_GRCm39v109=$CASTDIR/CAST_EiJ_full_sequence/Mus_musculus.GRCm39.109.dna.CAST
 NMASK_GRCm39v109=$CASTDIR/CAST_EiJ_N-masked/Mus_musculus.GRCm39.109.dna.NMASK.fa
 COORD_LIST=$CASTDIR/Nup_mm11_GRCm39_genecoord.txt
 OUT_FA=$CASTDIR/Nup_mm11_GRCm39_genecoord.txt.fa
+REF_base=`basename $REF_GRCm39v109 .fa`
+REF_sort=$WKDIR/GRCm39_ref/${REF_base}_sorted.fa
+CAST_base=`basename $CAST_GRCm39v109 .fa`
+CAST_sort=$CASTDIR/CAST_EiJ_full_sequence/${CAST_base}_sorted.fa
+NMASK_base=`basename $NMASK_GRCm39v109 .fa`
+NMASK_sort=$CASTDIR/CAST_EiJ_N-masked/${NMASK_base}_sorted.fa
+
 
 ## Import Nup107 peaks formatted as manually, remove ## comment out lines
 coords=($(cat $COORD_LIST))
@@ -28,10 +35,13 @@ module load samtools/1.14
 
 #`_ comment out if done before
 ## Samtools index .fa from Ref, Cast and N-masked GRCm38_68 snp-split generated genomes
-echo "Indexing REF, CAST and NMASK genomes"
-samtools index $REF_GRCm39v109
-samtools index $CAST_GRCm39v109
-samtools index $NMASK_GRCm39v109
+echo "Sorting and Indexing REF, CAST and NMASK genomes"
+samtools sort -o $REF_sort $REF_GRCm39v109
+samtools sort -o $CAST_sort $CAST_GRCm39v109
+samtools sort -o $NMASK_sort $NMASK_GRCm39v109
+samtools index $REF_sort
+samtools index $CAST_sort
+samtools index $NMASK_sort
 #`
 
 rm $OUT_FA
@@ -50,23 +60,23 @@ while IFS=$'\t' read -r -a rec; do
     echo "Processing Record NAME: " $NAME >> $OUT_FA
     echo ">REFB6" $NAME >> $OUT_FA
     if [ "$STRAND" -eq 1 ]; then
-    	samtools faidx $REF_GRCm39v109 $COORD >> $OUT_FA
+    	samtools faidx $REF_sort $COORD >> $OUT_FA
     	echo ">CASTEiJ_" $NAME >> $OUT_FA
-		samtools faidx $CAST_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx $CAST_sort $COORD >> $OUT_FA
 		echo ">NMASK_" $NAME >> $OUT_FA
-		samtools faidx $NMASK_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx $NMASK_sort $COORD >> $OUT_FA
 	elif [ "$STRAND" -eq -1 ]; then
-	    samtools faidx -i --mark-strand sign $REF_GRCm39v109 $COORD >> $OUT_FA
+	    samtools faidx -i --mark-strand sign $REF_sort $COORD >> $OUT_FA
     	echo ">CASTEiJ_" $NAME >> $OUT_FA
-		samtools faidx -i --mark-strand sign $CAST_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx -i --mark-strand sign $CAST_sort $COORD >> $OUT_FA
 		echo ">NMASK_" $NAME >> $OUT_FA
-		samtools faidx -i --mark-strand sign $NMASK_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx -i --mark-strand sign $NMASK_sort $COORD >> $OUT_FA
 	else
 		echo "Strand-Specifity not set, expecting 3 column tsv with name coord strand as input, Processing as + strand"
-		samtools faidx $REF_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx $REF_sort $COORD >> $OUT_FA
     	echo ">CASTEiJ_" $NAME >> $OUT_FA
-		samtools faidx $CAST_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx $CAST_sort $COORD >> $OUT_FA
 		echo ">NMASK_" $NAME >> $OUT_FA
-		samtools faidx $NMASK_GRCm39v109 $COORD >> $OUT_FA
+		samtools faidx $NMASK_sort $COORD >> $OUT_FA
 	fi
 done < $COORD_LIST
